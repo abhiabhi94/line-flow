f0784f3188ffa396cc98cba55c9aa2e417219da9
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2026-09-12 09:42:39 +0530

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.curious.lineflow"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
